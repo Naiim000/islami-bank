7a7ff205f4ed60cb5757e97fb2b0a33512959e9b
Naimur Rahman <139550206+Naiim000@users.noreply.github.com>
2023-08-29 23:49:41 +0600

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById('login-btn').addEventListener('click',function(){
     const email=emailField.value;
     const passwordField=document.getElementById('user-password')
     password=passwordField.value;
-    if(email=="naim@gmail.com" && password =="kireAppon")
+    if(email=="naim@gmail.com" && password =="naim00")
     {
         window.location.href="user.html";
     }
