ae019facfa08f0a8ea3e60856f3613342c8c4ca0
NAIIM0 <nrahman2162@gmail.com>
2023-08-29 12:18:55 +0600

    ok

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById('login-btn').addEventListener('click',function(){
     const email=emailField.value;
     const passwordField=document.getElementById('user-password')
     password=passwordField.value;
-    if(email=="naim@gmail" && password =="kireAppon")
+    if(email=="naim@gmail.com" && password =="kireAppon")
     {
         window.location.href="user.html";
     }
